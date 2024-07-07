74b03243ddf93c3b6370a17ae180edd993725647
JoshChico01 <joshuacicolari@gmail.com>
2024-07-07 13:10:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -52,18 +52,11 @@ chosen_images = []
 for img in images:
 
     left_col, divider_col, right_col = st.columns([0.2,0.02,0.78])
-
-    divider_col.markdown("""<style>
-            h2 {
-  border-left-style: solid;
-  border-left-color: gray;
-  padding-left: 25px;
-  }         
-            </style>""", unsafe_allow_html=True)
     
     divider_col.markdown("<h2 style='padding-bottom : 550px;'></h2>", unsafe_allow_html=True)
 
     left_col.image(img)
+    right_col.write("Select person:")
     
     with right_col:
         chosen_img = stis.image_select(label="",images=images, key = img)
