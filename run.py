c6508c4167a5ea346425020f3b38cefd1f2f4e2d
JoshChico01 <joshuacicolari@gmail.com>
2024-07-07 13:43:40 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ def done(chosen_images, images, name):
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
-        aws_access_key_id= os.environ['AWS_ACCESS_KEY_ID'] ,
-        aws_secret_access_key= os.environ['AWS_SECRET_ACCESS_KEY']
+        aws_access_key_id=  st.secrets["AWS_ACCESS_KEY_ID"], #os.environ['AWS_ACCESS_KEY_ID'] ,
+        aws_secret_access_key= st.secrets["AWS_SECRET_ACCESS_KEY"] #os.environ['AWS_SECRET_ACCESS_KEY']
     )
 
 
