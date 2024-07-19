232965df7e7afff8073b7bf084f8aba551828d05
JoshChico01 <joshuacicolari@gmail.com>
2024-07-19 13:54:19 +0000

    Fix bug

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ import random
 from io import StringIO
 
 def done(chosen_images, images, name, placeholder):
+    st.session_state.done = True
+
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
@@ -46,36 +48,40 @@ def getImgs():
 
     return images
 
+if "done" not in st.session_state:
+    st.session_state.done = False
+
 random.seed(10)
 
 placeholder = st.empty()
 
 images = getImgs()
 with placeholder.container():
-    st.header("Baby Photo Matching")
-    st.write("Match the baby photo with the person!")
+    if not st.session_state.done:
+        st.header("Baby Photo Matching")
+        st.write("Match the baby photo with the person!")
 
-    players = ["Sarah", "Shavani", "Yvonne", "Titia","Tester", "Pina", "Zoe", "Angela", "Amy I", "Tiff", "Courtney", "Megan", "Jess", "Renae", "Amie H", "Britt", "Jenny", "Gabs"]
+        players = ["Sarah", "Shavani", "Yvonne", "Titia","Tester", "Pina", "Zoe", "Angela", "Amy I", "Tiff", "Courtney", "Megan", "Jess", "Renae", "Amie H", "Britt", "Jenny", "Gabs"]
 
-    name = st.selectbox(label  = "Enter your name: ", options = players, key = "name" )
-    #name = st.text_input(label = "Enter your name:")
+        name = st.selectbox(label  = "Enter your name: ", options = players, key = "name" )
+        #name = st.text_input(label = "Enter your name:")
 
-    chosen_images = []
+        chosen_images = []
 
 
-    for img in images:
+        for img in images:
 
-        left_col, divider_col, right_col = st.columns([0.2,0.02,0.78])
-        left_col.image(img)
-        
-        with right_col:
-            img_options = [img_file.split(".")[-2] for img_file in os.listdir("./Images")]
-            random.shuffle(img_options)     
-            chosen_img = st.selectbox(label  = "Select: ", options = img_options, key = img )
-            chosen_images.append(chosen_img)
+            left_col, divider_col, right_col = st.columns([0.2,0.02,0.78])
+            left_col.image(img)
+            
+            with right_col:
+                img_options = [img_file.split(".")[-2] for img_file in os.listdir("./Images")]
+                random.shuffle(img_options)     
+                chosen_img = st.selectbox(label  = "Select: ", options = img_options, key = img )
+                chosen_images.append(chosen_img)
 
-        st.markdown("""---""")
+            st.markdown("""---""")
 
-    images = [img.split(".")[-2] for img in images]
+        images = [img.split(".")[-2] for img in images]
 
-    st.button("Done ✓", on_click=done, args=(chosen_images, images, name, placeholder))
+        st.button("Done ✓", on_click=done, args=(chosen_images, images, name, placeholder))
